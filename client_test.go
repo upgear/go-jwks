240079ad00357547825f00c3ee26cde8ad434759
duncan <duncan@wortharead.com>
2018-03-01 07:10:45 +0000

    Update to latest go-jose, add dependencies and fix test
    This commit updates the source to use the latest go-jose
    library. Dependency management through 'dep' has been added, to ensure
    people can always use this library, even as go-jose evolves.
    
    The unit test didn't do anything, so now it ensures the library can
    read example data from the relevant RFC.
    
    A .gitignore has been added to ensure the './vendor' directory doesn't
    get added to Git.

--- client_test.go
+++ client_test.go
@@ -4,23 +4,38 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-// TODO: Write actual test
 func TestNewClient(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		return
+		// Example data taken from RFC 7517
+		w.Write([]byte(`
+{"keys":
+   [
+	 {"kty":"EC",
+	  "crv":"P-256",
+	  "x":"MKBCTNIcKUSDii11ySs3526iDZ8AiTo7Tu6KPAqv7D4",
+	  "y":"4Etl6SRW2YiLUrN5vfvVHuhp7x8PxltmWWlbbM4IFyM",
+	  "use":"enc",
+	  "kid":"1"},
+
+	 {"kty":"RSA",
+	  "n": "0vx7agoebGcQSuuPiLJXZptN9nndrQmbXEps2aiAFbWhM78LhWx4cbbfAAtVT86zwu1RK7aPFFxuhDR1L6tSoc_BJECPebWKRXjBZCiFV4n3oknjhMstn64tZ_2W-5JsGY4Hc5n9yBXArwl93lqt7_RN5w6Cf0h4QyQ5v-65YGjQR0_FDW2QvzqY368QQMicAtaSqzs8KJZgnYb9c7d0zgdAZHzu6qMQvRL5hajrn1n91CbOpbISD08qNLyrdkt-bFTWhAI4vMQFh6WeZu0fM4lFd2NcRwr3XPksINHaQ-G_xBniIqbw0Ls1jF44-csFCur-kEgU8awapJzKnqDKgw",
+	  "e":"AQAB",
+	  "alg":"RS256",
+	  "kid":"2011-04-29"}
+   ]
+ }`))
 	}))
 
 	c, err := NewClient(ts.URL)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 
-	key, err := c.GetKey("abc")
-	if err != nil {
-		t.Fatal(err)
-	}
+	_, err = c.GetKey("2011-04-29")
+	assert.NoError(t, err)
 
-	_ = key
+	_, err = c.GetKey("doesn't exist")
+	assert.EqualError(t, err, "unrecognized key id")
 }
